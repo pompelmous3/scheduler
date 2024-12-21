dbdc34ced2c27fbd0dd0949b7b4d0a4f6b083483
Jack.Tseng <pompelmous429@gmail.com>
2024-12-21 16:22:32 -0700

    fix month roundup

--- main.cpp
+++ main.cpp
@@ -27,14 +27,26 @@ int main()
 	time_t tt = system_clock::to_time_t(now);
     struct tm *local_t = localtime(&tt);
 
+	// Get terminal size
 	int height, width;
 	getTerminalSize(height, width);
-	// TODO: decide how many displaying months accordingly
+	LOG("[main] (height, width)=(%d,%d)", height, width);
 
-	Month m = Month((*local_t).tm_year+1900, (*local_t).tm_mon+1, 5, 2,
-		height, width);
-	Month n = Month((*local_t).tm_year+1900, (*local_t).tm_mon+2, 50, 2,
-		height, width);
+	// Compose months
+	int cur_y, cur_m;
+	int next_y, next_m;
+	cur_y = (*local_t).tm_year+1900;
+	cur_m = (*local_t).tm_mon+1;
+	if (cur_m<12) {
+		next_y = cur_y;
+		next_m = cur_m+1;
+	} else {
+		next_y = cur_y+1;
+		next_m = 1;
+	}
+	// TODO: decide how many displaying months accordingly
+	Month m = Month(cur_y, cur_m, 5, 2, height, width);
+	Month n = Month(next_y, next_m, 50, 2, height, width);
 	Screen sc = Screen{height, width};
 	// Screen sc(height, width);
 	sc.addMonth(&m);
